f78da1d313a61b41e87425c0c4ebe43a9fc86548
kartikgits <kartik.kapparwan1@gmail.com>
2019-03-10 18:24:19 +0530

    Objective 3 - Add new Restaurant

--- vagrant/temp/webserver.py
+++ vagrant/temp/webserver.py
@@ -4,22 +4,39 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output=""
+                output+="<html><body>"
+                output+="<h1>Make a New Restaurant"
+                output+='''<form method="POST" enctype="multipart/form-data" action="/restaurants/new">'''
+                output+='''<input type="text" placeholder="New Restaurant Name" name="newrestaurantname">'''
+                output+='''<input type="submit" value="Create"></form>'''
+                output+="</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
             if self.path.endswith("/restaurants"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output=""
-                engine = create_engine('sqlite:///restaurantmenu.db')
-                Base.metadata.bind = engine
-                DBSession = sessionmaker(bind=engine)
-                session = DBSession()
                 output=""
                 output+="<html><body>"
+                #make a new restaurant link
+                output+="<a href='/restaurants/new'>Make a New Restaurant Here</a><br/><br/>"
                 restaurants = session.query(Restaurant).all()
                 for restaurant in restaurants:
                     output+="<div style='font-size:20px'>%s</div>" % restaurant.name
@@ -61,22 +78,20 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype=='multipart/form-data':
+                    fields=cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newrestaurantname')
+                    #new restaurant object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
         except:
             pass
 
